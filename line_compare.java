4b7166d44c4323713ee1d1700442f26b784a80de
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-03 09:18:01 +0530

    Update line_compare.java

--- line_compare.java
+++ line_compare.java
@@ -6,8 +6,13 @@ public class Line_Comparison {
 		int y1=4;
 		int x2=7;
 		int y2=1;
+		
+		public static void main(String[] args) 
+	{
+		System.out.println("Start with Displaying Welcome to Line Comparison Computation Program on Master Branch ");
+                System.out.println("************************************");
+
 	
-		double Length_Of_Line= Math.sqrt( ( ( x2 - x1 ) * ( x2 - x1 ) ) + ( ( y2 - y1 ) * ( y2 - y1 ) ) );
-				System.out.println(Length_Of_Line);
+		
 	}
 }
