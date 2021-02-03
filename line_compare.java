dc734789b16fa4d2296686930adca3246e5851eb
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-03 22:12:48 +0530

    Update line_compare.java

--- line_compare.java
+++ line_compare.java
@@ -1,18 +1,32 @@
-public class Line_Comparison {
+public class compare_line {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		int x1=3;
-		int y1=4;
-		int x2=7;
-		int y2=1;
-		
-		public static void main(String[] args) 
-	{
-		System.out.println(" Welcome to Line Comparison Computation Program");
-                System.out.println("************************************");
-
+		int x1=1;
+		int y1=3;
+		int x2=3;
+		int y2=7;
 	
+		double length1=Math.sqrt((x2-x1)*(x2-x1));
+		double length2=Math.sqrt((y2-y1)*(y2-y1)); 
 		
+		String obj1 = String.valueOf(length1);
+		String obj2 = String.valueOf(length2);
+		
+		double result=obj1.compareTo(obj2);
+		
+		 
+		if(result == 0)
+		{
+			System.out.println("Both line are same");
+		}
+		else if (result > 0 )
+		{
+				System.out.println("Line 1 is bigger than Line 2");
+		}
+		else
+			System.out.println("Line 2 is bigger than Line 1");
 	}
-}
+		
+			}         
+			
